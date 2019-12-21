ecf716e5b841c35731139d280609bf3199da4edd
Suite <a471486944@gmail.com>
2019-12-21 20:14:16 +0800

    Initial commit

---
+++ src/main/java/github/com/suitelhy/webchat/WebchatApplication.java
@@ -0,0 +1,13 @@
+package github.com.suitelhy.webchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/github/com/suitelhy/webchat/WebchatApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.suitelhy.webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
