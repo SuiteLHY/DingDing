88e1304a198a58bb1f152a20b7757eba417a4fb5
Suite <a471486944@gmail.com>
2020-01-11 15:49:26 +0800

    1. Entity 生成策略更新 - 相应调用实现调整

--- src/main/java/github/com/suitelhy/webchat/infrastructure/web/util/LogUtil.java
+++ src/main/java/github/com/suitelhy/webchat/infrastructure/web/util/LogUtil.java
@@ -1,6 +1,7 @@
 package github.com.suitelhy.webchat.infrastructure.web.util;
 
 import github.com.suitelhy.webchat.domain.entity.Log;
+import github.com.suitelhy.webchat.domain.vo.HandleTypeVo;
 
 public class LogUtil {
 
@@ -18,13 +19,25 @@ public class LogUtil {
             , Integer type
             , String detail
             , String ip) {
-        Log log = Log.Factory.SINGLETON.create();
+        /*Log log = Log.Factory.USER_LOG.create();
         log.setUserid(userid);
         log.setTime(time);
         log.setType(type);
         log.setDetail(detail);
-        log.setIp(ip);
-        return log;
+        log.setIp(ip);*/
+        HandleTypeVo.Log typeVo = null;
+        for (HandleTypeVo.Log each : HandleTypeVo.Log.class.getEnumConstants()) {
+            if (each.equalsValue(type)) {
+                typeVo = each;
+                break;
+            }
+        }
+        return Log.Factory.USER_LOG.create(null
+                , detail
+                , ip
+                , time
+                , typeVo
+                , userid);
     }
 
 }
